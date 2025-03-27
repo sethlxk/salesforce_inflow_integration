48f3f468908fc505587f8f66437bd62414c486eb
seth lee <sethlxk@gmail.com>
2025-03-27 16:28:17 +0800

    add sfdc order_id to inflow field

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def webhook():
         shippedDate = datetime.fromisoformat(f"{shippedDate}")
         time_difference = now - shippedDate
         if response["isCompleted"] == True and time_difference.total_seconds() <= 30:
-            logger.info(f"main: {sf.order_id}")
-            sf.update_order_status(sf.order_id)
+            order_id = response["customFields"]["custom1"]
+            sf.update_order_status(order_id)
     return {"status": 200}
 
 
